e3ebd64b2e8784aad8b40f0e0176d5e6498c9eba
Kassia Zheng <nz2280@nyu.edu>
2023-09-26 11:07:18 -0400

    fix all of the thing

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-let cDiam = 25;
-let num = 0
+let small = 25;
+let big = 50;
+
 let xOffset = 50;
 let xSpacing = 50;
 
@@ -14,22 +15,30 @@ function setup() {
 
 function draw() {
   background(234, 215, 0);
-  
+  fill(0);
 
   for (let xPos = 0; xPos <= width ; xPos += xSpacing) {
     for (let yPos = 0; yPos <= height ; yPos += ySpacing) {
-    if (xPos =xOffset + num * xSpacing){
 
-      ellipse(xPos, yPos, 100,100)}
-
-      else  {
-        ellipse(xPos, yPos, 50,50)}
-        
-    
-       
-      fill(0);
+        ellipse(xPos, yPos, small, small);
     }
-    num++;
+  
+  }
+
+  for (let xPos = 0; xPos <= width ; xPos += 100) {
+    for (let yPos = 0; yPos <= height ; yPos += 100) {
+
+        ellipse(xPos, yPos, big, big);
+    }
+  
+  }
+
+  for (let xPos = 50; xPos <= width ; xPos += 100) {
+    for (let yPos = 50; yPos <= height ; yPos += 100) {
+
+        ellipse(xPos, yPos, big, big);
+    }
+  
   }
 }
 
